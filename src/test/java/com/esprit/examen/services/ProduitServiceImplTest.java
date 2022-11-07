6ff5f1633aefa90e0eefcb1f3352ce891cc80e30
Mootaz <mootazfarwa23@gmail.com>
2022-11-07 18:14:08 +0100

    testunitaire

--- src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImplTest.java
@@ -1,51 +1,46 @@
 package com.esprit.examen.services;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
-import java.util.Date;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.verify;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.Optional;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.boot.test.mock.mockito.MockBean;
 import com.esprit.examen.entities.Produit;
+import com.esprit.examen.entities.Stock;
+import com.esprit.examen.repositories.CategorieProduitRepository;
+import com.esprit.examen.repositories.DetailFactureRepository;
 import com.esprit.examen.repositories.ProduitRepository;
-
-@SpringBootTest
+import com.esprit.examen.repositories.StockRepository;
+import springfox.documentation.spring.data.rest.OptionalDeferencer;
 public class ProduitServiceImplTest {
-	@Autowired
-	IProduitService  produitService;
-	@Autowired
-	ProduitRepository produitRepository;
-	@Test
-	public void testcreateproduit() {
-		
-			Produit po = new Produit("Mootaz","farwa",new Date(),new Date());
-		    Produit savedproduit= produitService.addProduit(po);	
-			assertNotNull(savedproduit.getLibelleProduit());
-			
-			
-		} 
 	
+	@MockBean
+    private CategorieProduitRepository categorieProduitRepository;
+    @MockBean
+    private ProduitRepository produitRepository;
+    @Autowired
+    private ProduitServiceImpl produitServiceImpl;
+    @MockBean
+    private StockRepository stockRepository;
 @Test
-public void testfindproduit() {
-	Produit p= produitRepository.findById(10L).get();
-	System.out.println(p);
-}
-
-@Test
-public void testupdateproduit() {
-	Produit p= produitRepository.findById(10L).get();
-	p.setPrix(2000);
-	produitRepository.save(p);
-	System.out.println(p);
-}
-
-
-@Test
-public void testFindAllproduit() {
-	List<Produit> prod=produitRepository.findAll();
+   public void testRetreiveById(){
+	Produit p = new Produit();
+	p.setCategorieProduit(null);
+	p.setCodeProduit("123");
+	p.setDateCreation(null);
+	p.setDateDerniereModification(null);
+	p.setDetailFacture(null);
+	p.setIdProduit(1L);
+	p.setLibelleProduit("libelle");
+	p.setPrix((float)5.500);
+	p.setStock(null);
+	Optional<Produit> produits = Optional.of(p);
+	when(produitRepository.findById((Long) any())).thenReturn(produits);
+	assertSame(p,produitServiceImpl.retrieveProduit(1L));
+	verify(produitRepository).findById((Long) any());
+	
 }
 }
