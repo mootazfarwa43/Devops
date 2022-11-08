e0d40d3e15a59b9d111f949da73198e7fadcf660
Mootaz <mootazfarwa23@gmail.com>
2022-11-08 11:36:14 +0100

    Nexus

--- src/main/java/com/esprit/examen/entities/Produit.java
+++ src/main/java/com/esprit/examen/entities/Produit.java
@@ -18,12 +18,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-
+@Getter
+@Setter
 
 public class Produit implements Serializable {
 
 	/**
-	 *  Produit DocString: Lorem ipsum
+	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -45,145 +46,60 @@ public class Produit implements Serializable {
 	@ManyToOne
 	@JsonIgnore
 	private CategorieProduit categorieProduit;
-	
-	
-
-	
-	
 	public Long getIdProduit() {
 		return idProduit;
 	}
-
-
-
-
 	public void setIdProduit(Long idProduit) {
 		this.idProduit = idProduit;
 	}
-
-
-
-
 	public String getCodeProduit() {
 		return codeProduit;
 	}
-
-
-
-
 	public void setCodeProduit(String codeProduit) {
 		this.codeProduit = codeProduit;
 	}
-
-
-
-
 	public String getLibelleProduit() {
 		return libelleProduit;
 	}
-
-
-
-
 	public void setLibelleProduit(String libelleProduit) {
 		this.libelleProduit = libelleProduit;
 	}
-
-
-
-
 	public float getPrix() {
 		return prix;
 	}
-
-
-
-
 	public void setPrix(float prix) {
 		this.prix = prix;
 	}
-
-
-
-
 	public Date getDateCreation() {
 		return dateCreation;
 	}
-
-
-
-
 	public void setDateCreation(Date dateCreation) {
 		this.dateCreation = dateCreation;
 	}
-
-
-
-
 	public Date getDateDerniereModification() {
 		return dateDerniereModification;
 	}
-
-
-
-
 	public void setDateDerniereModification(Date dateDerniereModification) {
 		this.dateDerniereModification = dateDerniereModification;
 	}
-
-
-
-
 	public Stock getStock() {
 		return stock;
 	}
-
-
-
-
 	public void setStock(Stock stock) {
 		this.stock = stock;
 	}
-
-
-
-
 	public Set<DetailFacture> getDetailFacture() {
 		return detailFacture;
 	}
-
-
-
-
 	public void setDetailFacture(Set<DetailFacture> detailFacture) {
 		this.detailFacture = detailFacture;
 	}
-
-
-
-
 	public CategorieProduit getCategorieProduit() {
 		return categorieProduit;
 	}
-
-
-
-
 	public void setCategorieProduit(CategorieProduit categorieProduit) {
 		this.categorieProduit = categorieProduit;
 	}
-
-
-
-
-	public Produit(long l, Object object, Object object2, Object object3, Object object4, Object object5, Object object6, Object object7, Object object8, Object object9) {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-
 	public Produit(Long idProduit, String codeProduit, String libelleProduit, float prix, Date dateCreation,
 			Date dateDerniereModification, Stock stock, Set<DetailFacture> detailFacture,
 			CategorieProduit categorieProduit) {
@@ -198,25 +114,14 @@ public class Produit implements Serializable {
 		this.detailFacture = detailFacture;
 		this.categorieProduit = categorieProduit;
 	}
-
-
-
-
-
-
-
-
-	@Override
-	public String toString() {
-		return "Produit [idProduit=" + idProduit + ", codeProduit=" + codeProduit + ", libelleProduit=" + libelleProduit
-				+ ", prix=" + prix + ", dateCreation=" + dateCreation + ", dateDerniereModification="
-				+ dateDerniereModification + "]";
+	public Produit(long l, Object object, Object object2, Object object3, Object object4, Object object5, Object object6, Object object7, Object object8, Object object9) {
+		super();
 	}
-
-
-
-
+	public Produit() {
+		// TODO Auto-generated constructor stub
+	}
 	
+
 
 	
 
