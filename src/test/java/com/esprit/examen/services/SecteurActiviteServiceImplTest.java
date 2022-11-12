338ce3989593ae97c2795389a933249043d05318
MedAmine1212 <khaili.amine@hotmail.fr>
2022-11-13 00:28:39 +0100

    fix tests

--- src/test/java/com/esprit/examen/services/SecteurActiviteServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/SecteurActiviteServiceImplTest.java
@@ -1,4 +1,4 @@
-package tn.esprit.rh.achat.tests;
+package com.esprit.examen.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
--- src/test/java/com/esprit/examen/services/StockServiceImplMock.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplMock.java
@@ -1,4 +1,4 @@
-package tn.esprit.rh.achat.tests;
+package com.esprit.examen.services;
 
 
 
